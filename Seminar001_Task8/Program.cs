08dff4de03fc93eae27d2e58aff7b66d4dea562c
Anna <Annagrex@rambler.ru>
2022-06-25 20:26:08 +0300

    add Seminar 001 Task 8

---
+++ Seminar001_Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿int n, current;
+Console.WriteLine("Введите число: ");
+n = Convert.ToInt32(Console.ReadLine());
+current = 2;
+while (current <= n)
+if (current%2 == 0)
+{
+    Console.Write(current + " ");
+    current = current + 2;
+}
