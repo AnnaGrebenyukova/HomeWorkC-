ecdd3d56536309c312f7b3e8c213591435ae4b3a
Anna <Annagrex@rambler.ru>
2022-06-25 20:28:54 +0300

    Äîáàâèëà îïèñàíèå çàäà÷è

--- Seminar001_Task8/Program.cs
+++ Seminar001_Task8/Program.cs
@@ -1,7 +1,14 @@
-﻿int n, current;
+﻿/*Задача 8: Напишите программу, которая 
+на вход принимает число (N), а на выходе 
+показывает все чётные числа от 1 до N.*/
+
+int n, current;
+
 Console.WriteLine("Введите число: ");
 n = Convert.ToInt32(Console.ReadLine());
+
 current = 2;
+
 while (current <= n)
 if (current%2 == 0)
 {
